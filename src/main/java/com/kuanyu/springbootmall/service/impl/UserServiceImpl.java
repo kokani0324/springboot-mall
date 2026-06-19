0f45f566c731d74d9fd62b4a929a904a528caa75
Gary Chen <33388620gary@gmail.com>
2026-06-19 16:18:49 +0800

    帳號功能 - 使用MD5 加密密碼

--- src/main/java/com/kuanyu/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/kuanyu/springbootmall/service/impl/UserServiceImpl.java
@@ -10,7 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class UserServiceImpl implements UserService {
@@ -35,6 +38,10 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        //使用MD5 生成密碼的雜湊值
+        String hashadPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashadPassword);
+        //創建帳號
         return userDao.createUsers(userRegisterRequest);
     }
 
@@ -47,10 +54,15 @@ public class UserServiceImpl implements UserService {
             log.warn("該email {}尚未註冊 ", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        //使用MD5 生成密碼的雜湊值
+        String hashadPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+
+
         //user.getPassword()資料庫的密碼
         //userLoginRequest.getPassword() 前端使用者的密碼判斷正不正確
         //一定要用equals來比較
-        if(user.getPassword().equals(userLoginRequest.getPassword())) {
+        if(user.getPassword().equals(hashadPassword)) {
             return user;
         }else {
             log.warn("email {} 的密碼不正確", userLoginRequest.getEmail());
